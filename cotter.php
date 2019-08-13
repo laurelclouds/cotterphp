e587cf66367f67b1d08853132fcafec23d8e992f
laurelclouds <laurelclouds@163.com>
2019-08-13 14:43:19 +0800

    版本0.1.1.0，按PSR-1、PSR-2标准格式化代码，完整实现PSR-4

--- cotter.php
+++ cotter.php
@@ -3,11 +3,15 @@
  * CotterPHP default autoloader & constant defines
  * Copyright 2019, laurelclouds@163.com
  */
+namespace cotter;
 
-if( !defined('COTTER_PHP_VERSION') )
+if (!defined('COTTERPHP_FRAMEWORK'))
 {
-    define('COTTER_PHP_VERSION', "0.1.0.0");
-    define('PHP_VERSION_REQUIRED', '5.4.0');
+    define('COTTERPHP_FRAMEWORK', __DIR__ . DIRECTORY_SEPARATOR . 'cotter' . DIRECTORY_SEPARATOR . 'CotterPHP.php');
+    define('COTTER_PHP_PATH', __DIR__);         // cotter.php 本文件所在目录
+
+    define('PHP_VERSION_REQUIRED', '5.4.0');    // 所需PHP最低版本号
+
 
     /* ************************************************************************* */
     // 检测服务器PHP版本，确定满足最小运行条件
@@ -15,43 +19,37 @@ if( !defined('COTTER_PHP_VERSION') )
     function php_version_id($version=PHP_VERSION)
     {
         $vers = explode('.', $version);
-        while(count($vers)<3) $vers[] = 0;
-        return ($vers[0] * 10000 + $vers[1] * 100 + $vers[2]);
+        while (count($vers)<3) $vers[] = 0;
+        return (intval($vers[0]) * 10000 + intval($vers[1]) * 100 + intval($vers[2]));
     }
-
-    define('COTTER_PHP_VERSION_ID', php_version_id(COTTER_PHP_VERSION));
 
     define('PHP_VERSION_ID_REQUIRED', php_version_id(PHP_VERSION_REQUIRED));
 
-    if(!defined('PHP_VERSION_ID'))
+    if (!defined('PHP_VERSION_ID'))
     {
         define('PHP_VERSION_ID', php_version_id(PHP_VERSION));
     }
 
-    if(PHP_VERSION_ID<PHP_VERSION_ID_REQUIRED) {
+    if (PHP_VERSION_ID<PHP_VERSION_ID_REQUIRED) {
         exit('PHP '.PHP_VERSION_REQUIRED.' or greater required.');
     }
 
-    /* ************************************************************************* */
-    // cotter.php 相关目录设置
-    /* ************************************************************************* */
-    // cotter.php 本文件所在目录
-    if(!defined('COTTER_PHP_PATH'))
-    {
-        define('COTTER_PHP_PATH', __DIR__);
-    }
-
-    // 默认的Autoloader, 置于所有其他的Autoloader（如果有的话）的最后
-    spl_autoload_register(function($class) {
-        if($class[0]=="\\") $class = substr($class, 1);
-        if(DIRECTORY_SEPARATOR!=="\\") $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
-        @include(COTTER_PHP_PATH . DIRECTORY_SEPARATOR . $class . '.php');
-    }, true, false);
-
     // 发现CotterPHP框架时，初始化CotterPHP框架
-    if(is_file(COTTER_PHP_PATH . DIRECTORY_SEPARATOR . 'cotter' . DIRECTORY_SEPARATOR . 'CotterPHP.php'))
+    if (is_file(COTTERPHP_FRAMEWORK))
     {
-        cotter\CotterPHP::initialize();
+        require(COTTERPHP_FRAMEWORK);
+        \cotter\CotterPHP::initialize();
+    }
+    else {
+        // 默认的Autoloader, 置于所有其他的Autoloader（如果有的话）的最后
+        spl_autoload_register(
+            function ($class) {
+                if ($class[0]=="\\") $class = substr($class, 1);
+                if (DIRECTORY_SEPARATOR!=="\\") $class = str_replace("\\", DIRECTORY_SEPARATOR, $class);
+                @include COTTER_PHP_PATH . DIRECTORY_SEPARATOR . $class . '.php';
+            },
+            true,
+            false
+        );
     }
 }
-?>
--- cotter/CotterPHP.php
+++ cotter/CotterPHP.php
@@ -2,168 +2,329 @@
 namespace cotter;
 
 /**
- * CotterPHP class
+ * CotterPHP class - PSR-4
  * 目的：注册定制的Autoloader、搭建CotterPHP框架运行环境、转交至恰当的应用程序入口
  */
 
 class CotterPHP
 {
     private static $instance = null;            // CotterPHP实例，应存在且仅存在一个实例
-    private static $mapStrategies = array();    // 映射策略的缓存数组
-    private static $namespaceMaps = array();    // 命名空间映射表
+    private static $vendors = array();          // 供应商映射策略
+    private static $namespaces = array();       // 命名空间映射表
+    private static $aliases = array();          // 类别名
+    private static $declarations = array();     // 特别声明，类名 => 对应文件；不使用通用规则的自动加载
 
-    private static $timeStart = 0;     // CotterPHP框架开始初始化的时间
+    protected static $timeStart = 0;            // CotterPHP框架开始初始化的时间
 
-    /**
-     * 驼峰式（camelCase, CamelCase）转短横线式（kebab-case）
-     * @param string $path      -- 需要转换的路径
-     * @return string
-     */
-    public static function camelToKebab($path)
-    {
-        if(empty($path)) return '';
-
-        $path = \preg_replace_callback(
-            '/.[A-Z]/',
-            function($matches) {
-                return $matches[0][0] . (in_array($matches[0][0], ['\\', '/']) ? '' : '-') .strtolower($matches[0][1]);
-            },
-            $path
-        );
-
-        $path = strtolower($path[0]) . substr($path, 1);
-    }
+    private $id = "";                           // 当前实例id（即供应商id）
+    private $strategy = false;                  // 当前实例的命名空间存放策略
 
     /**
-     * 短横线式（kebab-case）转驼峰式（camelCase或CamelCase）
-     * @param string $path          -- 需要转换的路径
-     * @param bool $bigCamel        -- 是否转换为大驼峰式（CamelCase）；true，大驼峰式(CamelCase)；false，小驼峰式camelCase；默认为小驼峰式camelCase
-     * @return $string
-     */
-    public static function kebabToCamel($path, $bigCamel=false)
-    {
-        if(empty($path)) return '';
-        $path = \preg_replace_callback(
-            '/(?:-[a-z])|(?:\\/[a-z])|(?:\\\\[a-z])/',
-            function($matches) use ($bigCamel) {
-                if($matches[0][0]=='-') {
-                    return strtoupper($matches[0][1]);
-                }
-
-                return $bigCamel ? \strtoupper($matches[0]) : $matches[0];
-            },
-            $path
-        );
-    }
-
-    /**
-     * include语句的函数写法
-     * @param string $file      -- 加载的文件名
-     * @return mixed            -- include语句的返回值
-     */
-    public static function import($file)
-    {
-        return @include $file;
-    }
-
-    /**
-     * 执行映射策略，获取映射结果
-     * @param string|array|function $strategy   -- 加载策略
-     * @param string $ns                        -- 查询的命名空间
-     * @return array                            -- 返回待搜索的路径的数组
-     */
-    private static function execStrategy(&$strategy, $ns='')
-    {
-        if(empty($strategy)) return array();
-
-        if(\is_array($strategy)) {
-            if(!isset($strategy[$ns])) return array();
-
-            $strategy = $strategy[$ns];
-        }
-
-        if(\is_callable($strategy)) $strategy = $strategy($ns);
-        if(\is_array($strategy)) return $strategy;
-
-        if(empty($strategy)) return array(COTTER_PHP_PATH);
-        return array($strategy);
-    }
-
-    /**
-     * 自动加载类名（traits等）对应文件
-     * @param string $class         -- 自动加载的类名（traits等）
+     * 自动加载类名等对应的文件
+     * @param string $class         -- 自动加载的类名等
      * @return void
      */
     public static function autoload($class)
     {
-        if($class[0]=="\\") $class = substr($class, 1);
-        $spaces = explode("\\", $class);
-        $n = count($spaces);
-
-        if($n<2) return;                            // 交给其他Autoloader处理
+        // 如果$class为别名，登记
+        if (isset(self::$aliases[$class])) {
+            return class_alias(self::$aliases[$class], $class, true);
+        }
         
-        $top = $spaces[0];
-        $ns = implode(DIRECTORY_SEPARATOR, array_slice($spaces, 0, $n-1));
-        $cls = $spaces[$n-1];
-
-        if(!isset(self::$namespaceMaps[$ns])) {
-            if(!isset(self::$mapStrategies[$top])) {
-                $strategyFile = __DIR__ . DIRECTORY_SEPARATOR . 'loaders' . DIRECTORY_SEPARATOR . $spaces[0] . '.loader.php';
-                if(!is_file($strategyFile)) return;     //加载策略不存在
-                self::$mapStrategies[$top] = @include $strategyFile;
-            }
-
-            self::$namespaceMaps[$ns] = self::execStrategy(self::$mapStrategies[$top], $ns);
+        $f = self::findClass($class);
+        if ($f!==false) {
+            @include $f;
+            return class_exists($class, false);
         }
 
-        if(!\is_array(self::$namespaceMaps[$ns])) return;
+        return false;
+    }
 
-        foreach(self::$namespaceMaps[$ns] as $dir) {
-            $fn = $dir . DIRECTORY_SEPARATOR . $cls . '.php';
-            if(is_file($fn)) {
-                return @include $fn;
-                break;
-            }
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = self::getVendor();
         }
+        return self::$instance;
     }
 
-    /**
-     * 注册自动加载
-     */
-    public static function register()
+    public static function getVendors()
     {
-        \spl_autoload_register(array('self', 'autoload'), false, true);
+        return self::$vendors;
     }
 
-    /**
-     * 解除自动加载
-     */
-    public static function unregister()
+    public static function getNamespaces()
     {
-        \spl_autoload_unregister(array('self', 'autoload'));
+        return self::$namespaces;
+    }
+
+    public static function getAliases()
+    {
+        return self::$aliases;
+    }
+
+    public static function getDeclarations()
+    {
+        return self::$declarations;
     }
 
     /**
      * CotterPHP框架初始化
+     * @return void
      */
     public static function initialize()
     {
-        if(!is_null(self::$instance)) return;
+        if (isset(self::$instance)) return;
+        self::$instance = self::getVendor("");
 
-        self::$timeStart = time();
-        self::register();
-
-        self::$instance = new CotterPHP();
+        \spl_autoload_register(array('self', 'autoload'), true, true);
     }
 
-    public function __construct()
+    /**
+     * 为指定命名空间添加查找目录
+     * @param string|array $namespace
+     * @param array|string $dirs
+     * @return void
+     */
+    public static function addNamespace($namespace, $dirs=null)
     {
-        
+        if (is_array($namespace)) {
+            self::$namespaces = array_merge_recursive(self::$namespaces, $namespace);
+            return;
+        }
+
+        if (substr($namespace, -1)!=="\\") $namespace .= "\\";
+
+        if (empty($dirs)) {
+            $dirs = [
+                \dirname(__DIR__) . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $namespace)
+            ];
+        }
+
+        if (!isset(self::$namespaces[$namespace]) || !is_array(self::$namespaces[$namespace])) {
+            self::$namespaces[$namespace] = array();
+        }
+
+        if (!is_array($dirs)) {
+            $dirs = [$dirs];
+        }
+        self::$namespaces[$namespace] = array_merge(self::$namespaces[$namespace], $dirs);
     }
 
-    public function __destruct()
+    /**
+     * 为类添加别名
+     * @param string|array $alias
+     * @param string $class
+     * @return void
+     */
+    public static function addAlias($alias, $class=null)
     {
-        
+        if (is_array($alias)) {
+            self::$aliases = array_merge(self::$aliases, $alias);
+            return;
+        }
+
+        if (empty($class)) {
+            $class = "cotter\\$alias";
+        }
+
+        self::$aliases[$alias] = $class;
     }
 
+    /**
+     * 指定类声明所在的具体PHP文件
+     * @param string|array $class
+     * @param string $filename
+     * @return void
+     */
+    public static function addDeclaration($class, $filename=null)
+    {
+        if (is_array($class)) {
+            self::$declarations = array_merge(self::$declarations, $class);
+            return;
+        }
+
+        if (empty($filename)) {
+            $filename = dirname(__DIR__) . DIRECTORY_SEPARATOR . str_replace("\\", DIRECTORY_SEPARATOR, $class) . ".php";
+        }
+
+        self::$declarations[$class] = $filename;
+    }
+
+    /**
+     * 获取指定供应商
+     * @param string $vendorId      -- 供应商id
+     * @return CotterPHP
+     */
+    protected static function getVendor($vendorId = "")
+    {
+        if (!isset(self::$vendors[$vendorId])) {
+            self::$vendors[$vendorId] = new static($vendorId);
+        }
+
+        return self::$vendors[$vendorId];
+    }
+
+    /**
+     * 查找类
+     * @param string $class
+     * @return false|string
+     */
+    protected static function findClass($class)
+    {
+        $at = strpos($class, "\\");
+        $vendorId = "";
+        if ($at!==false) {
+            $vendorId = substr($class, 0, $at);
+        }
+
+        $vendor = self::getVendor($vendorId);
+        $files = $vendor->lookup($class);
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                return $file;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * CotterPHP 构造函数
+     * @access protected
+     * @param string $namespace
+     * @return void
+     */
+    protected function __construct($vendorId = "")
+    {
+        if (!self::$timeStart) self::$timeStart = time();
+
+        $strategy = ["\\"   => \dirname(__DIR__) . DIRECTORY_SEPARATOR];
+        if (!empty($vendorId)) {
+            $strategyFile = __DIR__ . DIRECTORY_SEPARATOR .
+                "loaders" . DIRECTORY_SEPARATOR .
+                $vendorId . ".loader.php";
+            if (is_file($strategyFile)) {
+                $strategy = @include $strategyFile;
+            }
+            else {
+                // 交给默认策略进行处理
+                $strategy = false;
+            }
+        }
+
+        $this->id = $vendorId;
+        $this->strategy = $strategy;
+    }
+
+    /**
+     * 执行策略
+     * @param string $namespace
+     * @return array
+     */
+    protected function execStrategy($namespace)
+    {
+        if (isset(self::$namespaces[$namespace])) {
+            return self::$namespaces[$namespace];
+        }
+
+        $strategy = $this->strategy;
+        $empty = empty($strategy);
+        $result = array();
+        if (is_callable($strategy)) {
+            // 策略为函数求值，求值结果：false|array|string
+            $t = $strategy($namespace);
+            if (is_string($t)) {
+                $result[] = $t;
+            }
+            elseif (is_array($t)) {
+                $result = array_merge($result, $t);
+            }
+        }
+        elseif (is_array($strategy) && !$empty) {
+            // 策略为数组查询，查询结果：array|string
+            $l = strlen($namespace);
+            $offset = -1;
+            while (false!==($at=strrpos($namespace, "\\", $offset))) {
+                $ns = substr($namespace, 0, $at+1);
+                if (isset($strategy[$ns])) {
+                    $t = $strategy[$ns];
+                    if (!\is_array($t)) {
+                        $t = [ $t ];
+                    }
+                    if ($ns!==$namespace) {
+                        $suffix = substr($namespace, strlen($ns));
+                        $n = count($t);
+                        for ($i=0; $i<$n; $i++) {
+                            $t[$i] .= str_replace("\\", DIRECTORY_SEPARATOR, $suffix);
+                        }
+                    }
+
+                    $result = array_merge($result, $t);
+                }
+
+                $offset = $at - $l - 1;
+            }
+        }
+        elseif (!$empty) {
+            // 策略为根据指定供应商目录求值，求值结果：string
+            $result[] = $strategy . str_replace(
+                "\\",
+                DIRECTORY_SEPARATOR,
+                substr($namespace, strlen($this->id)+1)?:""
+            );
+        }
+        
+        $defaultDir = dirname(__DIR__) . DIRECTORY_SEPARATOR;
+
+        $result = array_merge(
+            $result,
+            array($defaultDir . str_replace("\\", DIRECTORY_SEPARATOR, $namespace))
+        );
+
+        self::$namespaces[$namespace] = $result;
+        return $result;
+    }
+
+    /**
+     * 查找命名空间或类对应的目录或文件
+     * @param string $noc       -- Namespace Or Class(noc)
+     * @return array
+     */
+    public function lookup($noc = "")
+    {
+        if (empty($noc)) {
+            $noc = $this->id . "\\";
+        }
+        $at = strrpos($noc, "\\");
+        $ns = "\\";
+        $class = $noc;
+
+        if ($at!==false) {
+            $at ++;
+            $ns = substr($noc, 0, $at);
+            $class = substr($noc, $at);
+        }
+
+        if (!empty($class)) {
+            // 如果某类已明确声明使用特定文件，则返回指定文件构成的数组
+            if (isset(self::$declarations[$noc])) {
+                return [self::$declarations[$noc]];
+            }
+        }
+
+        // 查找指定命名空间对应目录列表
+        $result = $this->execStrategy($ns);
+        if (!empty($class)) {
+            // 按通用规则查找指定类的可能文件列表
+            $result = array_map(
+                function ($v) use ($class) {
+                    return $v . $class . ".php";
+                },
+                $result
+            );
+        }
+
+        return $result;
+    }
 }
